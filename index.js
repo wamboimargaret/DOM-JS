dc8587fbcebfc91e2f145bf785152aa8aae6f31c
wamboimargaret <margarettwamboi@gmail.com>
2023-05-22 19:37:46 +0300

    Styling with DOM

---
+++ index.js
@@ -0,0 +1,30 @@
+document.getElementById('title').style.color='green';
+document.getElementById('title').style.textAlign = 'center'
+document.body.style.backgroundColor='silver';
+document.body.style.fontFamily = 'Arial'
+document.body.style.fontSize = '20px'
+document.getElementById('paragraph').style.fontSize = '20px'
+document.getElementById('paragraph').style.textAlign = 'center'
+document.getElementById('h3tag').style.textTransform = "uppercase"
+document.getElementById('h3tag2').style.textTransform = "uppercase"
+document.getElementById('h3tag').style.color = 'green'
+document.getElementById('h3tag2').style.color = 'green'
+
+let list = document.createElement('li');
+list.innerHTML = 'Orange'
+document.getElementById('fruList').appendChild(list);
+
+let newveg = document.createElement('li')
+newveg.innerHTML = 'Brocolli',
+document.getElementById('vegList').appendChild(newveg);
+
+document.getElementById('container').style.border=" thin solid black";
+document.getElementById('container').style.borderRadius = '20px'
+document.getElementById('container').style.padding = '30px'
+
+
+
+
+
+
+ 
